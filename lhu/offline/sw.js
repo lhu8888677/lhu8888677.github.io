b1b8730f0f429cc643affa8eb68dae9c0a4ba2a2
lhu8888677 <lhu8888677@gmail.com>
2025-08-12 01:24:21 +0800

    Add files via upload

--- lhu/offline/sw.js
+++ lhu/offline/sw.js
@@ -1,5 +1,8 @@
-const CACHE_NAME='rel-offline-20250811164148';
-const ASSETS=['./','./index.html','./manifest.json','./disclaimer.html','./offline.html','./logo.png','./icons/icon-192.png','./icons/icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./offline.html'))));});
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open('v1').then(cache => cache.addAll([
+    './index.html'
+  ])));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));
+});
