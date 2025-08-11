b9746d0566f453c3d9c91b0c56213f0abf36e0ae
lhu8888677 <lhu8888677@gmail.com>
2025-08-11 22:18:24 +0800

    Add files via upload

--- lhu/offline/sw.js
+++ lhu/offline/sw.js
@@ -1,20 +1,30 @@
-self.addEventListener('install', function(event) {
+const CACHE_NAME = 'lhu-offline-v20250811';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './offline.html',
+  './logo.png',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.open('pwa-cache').then(function(cache) {
-      return cache.addAll([
-        '/lhu/offline/index.html',
-        '/lhu/offline/offline.html',
-        '/lhu/offline/icons/logo.png'
-      ]);
-    })
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
   );
 });
-self.addEventListener('fetch', function(event) {
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
   event.respondWith(
-    fetch(event.request).catch(function() {
-      return caches.match(event.request).then(function(response) {
-        return response || caches.match('/lhu/offline/offline.html');
-      });
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+      return fetch(event.request).catch(() => caches.match('./offline.html'));
     })
   );
 });
