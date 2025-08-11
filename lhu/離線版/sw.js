d22c0a64b0f68db49c8fd7e30b5f229ef2d7a5b1
lhu8888677 <lhu8888677@gmail.com>
2025-08-12 00:23:49 +0800

    Add files via upload

--- lhu/離線版/sw.js
+++ lhu/離線版/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'zh-final-20250811151056';
-const ASSETS = ['./','./index.html','./manifest.json','./offline.html','./logo.png','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE_NAME = 'zh-final-disc-20250811155926';
+const ASSETS = ['./','./index.html','./manifest.json','./offline.html','./disclaimer.html','./logo.png','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install', e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate', e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))));
 self.addEventListener('fetch', e=>{ if(e.request.method!=='GET') return; e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).catch(()=>caches.match('./offline.html')))); });
